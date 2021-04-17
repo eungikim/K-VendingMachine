d35274fe2d5e31a03286d6c1507047e45f3a1a9b
eungikim <eungikim.me@gmail.com>
2021-04-17 17:58:43 +0900

    Add ml property

--- src/main/kotlin/Beverage.kt
+++ src/main/kotlin/Beverage.kt
@@ -1,4 +1,7 @@
-data class Beverage (val name: String, var price: Int) {
+data class Beverage (val name: String, var price: Int, val ml: Int) {
     val taste: String? = null
 
+    override fun toString(): String {
+        return "$name(${ml}ml) : ${price}원"
+    }
 }
